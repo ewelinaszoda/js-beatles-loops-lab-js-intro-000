72b716841096af77ccd9feb49801094936e46ebb
Ewelina Szoda <ewelinaszoda@gmail.com>
2020-03-31 07:22:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // add solution here
 function johnLennonFacts(array) {
-  array = [];
+  let Newarray = [];
   for ( let i = 0; i < array.length; i++) {
     array[i] = array[i] + "!!!";
-  } return array;
+  } return Newarray;
 }
